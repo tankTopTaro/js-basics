c00f9c4071eac8fa36b78edbf17415f9c3ae63c5
Kevin Balmores <balmoresk3v1n@gmail.com>
2023-03-03 21:04:16 +0800

    variable declaration

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+var a;          // variable declaration
+var b = 10;     // variable initialization
+
+var myName;
+var myName = "Kevin";
+
+console.log(b)
+console.log(typeof(b))
+console.log(myName)
